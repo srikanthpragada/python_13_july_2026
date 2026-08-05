2c0950460dc61b1cdbf51534bb64518339117f97
srikanthpragada <srikanthpragada@gmail.com>
2026-08-05 20:54:24 +0530

    05-AUG

---
+++ demo/oop/account.py
@@ -0,0 +1,30 @@
+class Account:
+    # Constructor
+    def __init__(self, acno, ahname, balance = 0):
+        # Object Attributes
+        self.acno = acno
+        self.ahname = ahname
+        self.balance = balance
+
+    # Methods
+    def getbalance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+
+    # Create an object of Account
+a1 = Account(1, "Gary", 10000)
+a1.withdraw(2000)
+a1.deposit(5000)
+print(a1.getbalance())
+#print(a1.balance)  # not to be done
+
+
+a2 = Account(2, 'Ben')
+print(a2.getbalance())
+
---
+++ demo/oop/counter.py
@@ -0,0 +1,35 @@
+class Counter:
+    def __init__(self, value = 1):
+        #Object Attributes
+        self.value = value
+        self.initvalue = value
+
+    def inc(self):
+        self.value += 1
+
+    def dec(self):
+        self.value -= 1
+
+    def getvalue(self):
+        return self.value
+
+    def reset(self):
+        self.value = self.initvalue
+
+    def __str__(self):
+        return f"Value = {self.value}"
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+c = Counter(100)
+print(c)  # c.__str__()
+
+c2 = Counter(100)
+print(c == c2) # c.__eq__(c2)
+
+# c.inc()
+# c.inc()
+# print(c.getvalue())
+# c.reset()
+# print(c.getvalue())
